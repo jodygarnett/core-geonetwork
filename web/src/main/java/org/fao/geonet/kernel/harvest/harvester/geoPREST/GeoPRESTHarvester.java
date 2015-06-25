7a6eea1637b7165f7009a80c4dcd355c1c6c47af
Aaron Sedgmen <aaron.sedgmen@ga.gov.au>
2015-06-25 15:32:24 +1000

    Updates to close DB connections when harvest is finished.

--- web/src/main/java/org/fao/geonet/kernel/harvest/harvester/geoPREST/GeoPRESTHarvester.java
+++ web/src/main/java/org/fao/geonet/kernel/harvest/harvester/geoPREST/GeoPRESTHarvester.java
@@ -218,10 +218,18 @@ public class GeoPRESTHarvester extends AbstractHarvester
 
 	protected void doHarvest(Logger log, ResourceManager rm) throws Exception
 	{
-		Dbms dbms = (Dbms) rm.open(Geonet.Res.MAIN_DB);
 
-		Harvester h = new Harvester(log, context, dbms, params);
-		result = h.harvest();
+		Dbms dbms = null;
+		
+		try {
+			dbms = (Dbms) rm.openDirect(Geonet.Res.MAIN_DB);
+
+			Harvester h = new Harvester(log, context, dbms, params);
+			result = h.harvest();
+		} finally {
+			if (dbms != null) rm.close(Geonet.Res.MAIN_DB, dbms);
+		}
+
 	}
 
 	//---------------------------------------------------------------------------
