df702f0378c825beb1af9f359ddd71640fa702ea
Antonio C <antonio.cerciello@geocat.net>
2018-12-20 10:42:32 +0100

    Add missing elastic files and upgrade to 3.4.4-1

--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v344/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v344/migrate-default.sql
@@ -1,4 +1,4 @@
 UPDATE Settings set value = 'http://www.openbasiskaart.nl/mapcache/%3FLAYERS=osm-nb%26SERVICE=WMS%26VERSION=1.1.1%26REQUEST=GetMap%26STYLES=%26FORMAT=image%2Fpng%26EXCEPTIONS=application%2Fvnd.ogc.se_inimage%26SRS={SRS}%26BBOX={MINX},{MINY},{MAXX},{MAXY}%26WIDTH={WIDTH}%26HEIGHT={HEIGHT}' WHERE name = 'region/getmap/background';
 
 UPDATE Settings SET value='3.4.4' WHERE name='system/platform/version';
-UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
+UPDATE Settings SET value='1' WHERE name='system/platform/subVersion';
--- web/src/main/webapp/WEB-INF/classes/setup/sql/migrate/v345/migrate-default.sql
+++
@@ -1,2 +0,0 @@
-UPDATE Settings SET value='3.4.5' WHERE name='system/platform/version';
-UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
